b92585cd39e6873135070dafb3365134ecaa9cb4
Batouuuuu <gillet.baptiste94@gmail.com>
2024-01-23 17:10:46 +0100

    modif donnees.py

--- donnees/donnees.py
+++ donnees/donnees.py
@@ -1,30 +1,53 @@
 import csv
 
-def players(path):
 
-    player_name = input("User : ").title().strip()
+###pour des raisons de simplicité le fichier csv contient 2 colonnes la première USER et l'autre Score, c'est intitulés ne figurent pas car problème pour extraire les données sinon
+
+def print_score(path, player_name):
+    """"Affichage des scores et appel de la fonction get_best_player"""
+
     score = 0
-
-    existing_players = [] 
+    existing_players = []
 
     with open(path, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|') 
 
         for row in csv_reader:
-            best_player = row["Score"]
-            print("Le meilleur joueur actuellement est : avec un score de ")
-            if player_name == row["User"]:
-                existing_players.append(row["User"]) # ajout du nom du joueur à la liste des joueurs existants
-                print(f"Bon retour parmi nous {row['User']} ton score était de {row['Score']} points")
             
-        if player_name not in existing_players:
-            with open(path, 'a', newline='') as csvfile:
-                print(f"Bienvenue parmi-nous nouveau joueur : {player_name}")
-                new_player = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                new_player.writerow((player_name,score)) #utilisation d'un tuple car writerow ne prend qu'un argument
+            if player_name == row[0]:
+                existing_players.append(row[0]) # ajout du nom du joueur à la liste des joueurs existants
+                print(f"Bon retour parmi nous {row[0]} ton score était de {row[1]} points")
+            get_best_player(row, score)
+
+                
+            
+            if player_name not in existing_players:
+                create_new_player(path, player_name, score)
     
-    return
-        
-players(".//data_players.csv")
+    
+ 
+
+def get_best_player(row, score):
+    """"Obtenir le joueur qui a le meilleur score et convertir le colonne des scores en int"""
+
+    convert = int(row[1]) # conversion des scores en integer           
+    if convert > score:
+        score = convert
+        get_best_user = row[0]
+        print(f"Le meilleur joueur actuellement est : {get_best_user} avec un score de {score} ")
+    return score
 
 
+def create_new_player(path, player_name, score):
+    """"Création d'un nouveau joueur"""
+
+    with open(path, 'a', newline='') as csvfile:
+        print(f"Bienvenue parmi-nous nouveau joueur : {player_name}")
+        new_player = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        new_player.writerow((player_name,score)) #utilisation d'un tuple car writerow ne prend qu'un argument
+
+
+def start_game():
+    player_name = input("User : ").title().strip()
+    print_score("./donnees/data_players.csv", player_name) 
+start_game()
